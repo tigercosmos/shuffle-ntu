4f139d25418ce50b0253a0ae237de12a5fe1ee4b
tigercosmos <b04209032@ntu.edu.tw>
2017-08-23 18:32:10 +0800

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule.forRoot()
+  ],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
