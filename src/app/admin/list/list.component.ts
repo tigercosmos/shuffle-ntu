3c73fa9fab4aa23e56555fa91e839d1bf9a92d82
tigercosmos <b04209032@ntu.edu.tw>
2017-09-28 15:16:50 +0800

    list

---
+++ src/app/admin/list/list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  usersDB: FirebaseObjectObservable<any>;
+  data: Array<any> = []; // table data
+  user: any = {};
+
+  constructor(private db: AngularFireDatabase) {
+    this.usersDB = db.object('/users');
+    this.usersDB.subscribe(items => {
+      console.log(items);
+      for (const i in items) {
+        if (items.hasOwnProperty(i)) {
+          this.data.push(items[i]);
+        }
+      }
+      console.log(this.data);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
